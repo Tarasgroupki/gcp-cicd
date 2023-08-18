66d0d7d20676777099d47fe1d8e7817f8adfece2
Tarasgroupki <kryvtsun16061996@gmail.com>
2023-08-18 22:34:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-    const message="<font color='blue'>СloudFunction of Taras Kryvtsun!</font><br><b>App Version 1.2.4</b>";
+    const message="<font color='blue'>СloudFunction of Taras Kryvtsun!</font><br><b>App Version 1.3.1</b>";
     res.status(200).send(message);
 };
